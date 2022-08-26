55b992590c73382ccb35cf2f6f566b8a2c6ebd5b
srgo211 <novikovsn87@gmail.com>
2022-08-26 21:45:03 +0300

    Добавьте файлы проекта.

---
+++ App.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows;
+using Telerik.Windows.Controls;
+using WpfTelerikLocalization.Localization;
+
+namespace WpfTelerikLocalization
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public App()
+        {
+            LocalizationManager.Manager = new LocalizationManager()
+            {
+                ResourceManager = FilteringUIResources.ResourceManager,
+                Culture = new System.Globalization.CultureInfo("ru"),
+            };
+            //InitializeComponent();
+
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,17 @@
+﻿using BindingColumnsFromViewModel;
+using System.Windows;
+
+namespace WpfTelerikLocalization
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = new MyDataContext();
+        }
+    }
+}
